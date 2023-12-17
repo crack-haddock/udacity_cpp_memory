9397d21e5d4f390875ecae8dc5a4f2a03cba29f1
crack-haddock <richardmoss@outlook.com>
2023-12-17 15:31:26 +0000

    fix bug described in warm up by not deleting chatbot in graph node destructor

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -11,7 +11,8 @@ GraphNode::~GraphNode()
     //// STUDENT CODE
     ////
 
-    delete _chatBot; 
+    //if (_chatBot != nullptr)
+      //  delete _chatBot; 
 
     ////
     //// EOF STUDENT CODE
